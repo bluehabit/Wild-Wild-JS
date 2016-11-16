203afe9f1658d25e6e91e863ab326d58f574c203
Chris Feltus <chris.feltus@gmail.com>
2016-11-15 19:30:13 -0600

    added more content to index.html

--- snakes.js
+++ snakes.js
@@ -1,16 +1,18 @@
-function filter(arr, callback){
-	var result = [];
+// function filter(arr, callback){
+// 	var result = [];
 
-	for(var i = 0; i < arr.length; i++){
-		result.push(arr[i]);
-	}
+// 	for(var i = 0; i < arr.length; i++){
+// 		result.push(arr[i]);
+// 	}
 
-	return result; 
-}
+// 	return result; 
+// }
 
 
-var numbers = [1,2,3,4,5];
+// var numbers = [1,2,3,4,5];
 
-var myVar = filter(numbers, function(){
+// var myVar = filter(numbers, function(){
 
-});
+// });
+
+//Anonymous function
